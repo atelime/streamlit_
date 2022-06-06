a7c6ab80cd31a72e14c0605b8155b4957aa6f7ab
atelime <atelierlime@gmail.com>
2022-06-06 16:45:08 +0900

    Add files via upload

--- main_page.py
+++ main_page.py
@@ -2,3 +2,15 @@ import streamlit as st
 
 st.markdown("# Main page 🎈")
 st.sidebar.markdown("# Main page 🎈")
+
+import seaborn as sns
+
+mpg = sns.load_dataset("mpg")
+
+st.write("""
+### 자동차 연비
+""")
+
+st.dataframe(mpg)
+
+st.line_chart(mpg["mpg"])
